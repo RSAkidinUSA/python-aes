1d3783f706fa5cfdfd8415629723061bc8f54ed2
Ryan Burrow <rsardb11@vt.edu>
2017-11-14 09:50:17 -0500

    key length computation works, added basics for sbox

--- aes_expansion.py
+++ aes_expansion.py
@@ -2,6 +2,22 @@
 # aes_expansion.py code for computing the key expansion for aes
 from aes_inv import inverse
 
+# provide the user with a hex key of one of the correct lengths
+def expand(key):
+    try:
+        test = int(key, 16)        
+    except:
+        print("Invalid input type. Please enter hex value.")
+        return 1
+    keylen = len(key) * 4
+    if (keylen != 128) and (keylen != 192) and (keylen != 256):
+        print("Invalid key length. Please use 128, 192, or 256 bit key.")
+        return 2
+    numRounds = 10 + int((keylen - 128) / 32)
+    expKeyLen = (numRounds + 1) * 16
+    print("Key length: %d\nNumber of rounds %d:\nExpanded key length: %d" \
+          %(keylen, numRounds, expKeyLen))
+    
 def main():
     print("Please don't run this as main")
     
---
+++ aes_sbox.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+# aes_sbox.py code for sboxes for aes
+
+# take a hex byte and return the sbox substitution for it.
+# optional argument allows for reversal with decryption
+def sub(byte, encrypt = True):
+    return byte
+
+def main():
+    print("Please don't run this as main")
+    
+
+if __name__=='__main__':
+    main()
