8412a450308b7338c0b002f990d1e9dd698942f0
Ryan Burrow <rsardb11@vt.edu>
2017-11-16 10:04:14 -0500

    added output file optional argument

--- aes_main.py
+++ aes_main.py
@@ -3,18 +3,32 @@
 from aes_expansion import expand
 import argparse
 
+class SmartFormatter(argparse.HelpFormatter):
+
+    def _split_lines(self, text, width):
+        if text.startswith('R|'):
+            return text[2:].splitlines()  
+        # this is the RawTextHelpFormatter._split_lines
+        return argparse.HelpFormatter._split_lines(self, text, width)
+
 def get_key(fileName):
     return '000102030405060708090a0b0c0d0e0f'
 
 def parse():
     parser = argparse.ArgumentParser(description='Encrypt or Decrypt a file'\
-                                     ' with AES128, AES192, or AES256.')
-    parser.add_argument('key', metavar='key', type=str, nargs=1,
+                                     ' with AES128, AES192, or AES256.',
+                                     formatter_class=SmartFormatter)
+    parser.add_argument('key_file', type=str, nargs=1,
                         help='The file storing the 128, '\
                         '192, or 256 bit key as text')
-    parser.add_argument('-f', metavar='input', type=str, nargs=1,
-                        help='The file storing the text to be '\
-                        '(en/de)crypted')
+    parser.add_argument('-f', metavar='input_file', type=str, nargs=1,
+                        help='R|The file storing the text to be '\
+                        '(en/de)crypted.\n'\
+                        'If not used, input must come from stdin.')
+    parser.add_argument('-o', metavar='output_file', type=str, nargs=1,
+                        help='R|The file to store the '\
+                        '(en/de)crypted text in.\n'\
+                        'If not used, output will go to stdout.')
     args = parser.parse_args()
 
 def main():
