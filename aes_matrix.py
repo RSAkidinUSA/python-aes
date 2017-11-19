02cb6a8390f1443c1f0a0db23afad6efa17fcec5
Ryan Burrow <rsardb11@vt.edu>
2017-11-19 11:59:46 -0500

    created matToStr and strToMat functions

--- aes_matrix.py
+++ aes_matrix.py
@@ -4,12 +4,27 @@
 # shift rows
 # data is stored in array of arrays
 # first index is row, second is column
-def shift_rows(data, encrypt=True):
+
+# convert strig to matrix
+def strToMat(data):
     ret = [[],[],[],[]]
     # copy values into the matrix
     for i in range(4):
         for j in range(4):
             ret[j].append(data[(4*i*2)+(j*2):(4*i*2)+(j*2)+2])
+    return ret
+    
+# convert matrix to string
+def matToStr(data):
+    ret = ''
+    for i in range(4):
+        for j in range(4):
+            ret = ret + data[j][i]
+    return ret
+
+def shift_rows(data, encrypt=True):
+    # convert string to matrix
+    ret = strToMat(data)
     # shift
     for i in range(1, 4):
         for j in range(i):
@@ -19,37 +34,33 @@ def shift_rows(data, encrypt=True):
             else:
                 temp = ret[i].pop(3)
                 ret[i].insert(0, temp)
+    # convert matrix back to string 
+    ret = matToStr(ret)
     return ret
 
 # mix columns
 def mix_cols(data, encrypt=True):
-
+    # convert string to matrix
+    ret = strToMat(data)
     # convert matrix back to string
-    ret = ''
-    for i in range(4):
-        for j in range(4):
-            ret = ret + data[j][i]
+    ret = matToStr(ret)
     return ret
 
 def main():
     # Test shift and mix
     data_start = '00102030011121310212223203132333'.upper()
     data = data_start
-    print("Data pre shift:\t%s" % (data,))
+    print("Data pre shift: \t%s" % (data,))
     data = shift_rows(data)
-    print("Matrix: post shift:")
-    for i in range(len(data)):
-        print(data[i])
+    print("Data post shift:\t%s" % (data,))
     data = mix_cols(data)
-    print("Data post mix:\t%s" % (data,))
+    print("Data post mix:  \t%s" % (data,))
     # Test inverse shift and mix
-    print("Data pre shift:\t%s" % (data,))
+    print("Data pre shift: \t%s" % (data,))
     data = shift_rows(data, False)
-    print("Matrix: post shift:")
-    for i in range(len(data)):
-        print(data[i])
+    print("Data post shift:\t%s" % (data,))
     data = mix_cols(data, False)
-    print("Data post mix:\t%s" % (data,))
+    print("Data post mix:  \t%s" % (data,))
     print("Input == Output: %s" % (data == data_start,))
 
 if __name__ == '__main__':
